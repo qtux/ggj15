fef6592ac78bccb8cabd92cb8945c3ed3383bae8
Sebastian Artz <sebastian.artz85@gmail.com>
2015-01-25 00:47:29 +0100

    new scaling

--- src/Tile.hpp
+++ src/Tile.hpp
@@ -12,6 +12,6 @@ public:
 	virtual void update(sf::Time deltaTime);
 	const static int pixelSizeX = 8;
 	const static int pixelSizeY = 8;
-	const static int tileScaleFactor = 2;
+	const static int tileScaleFactor = 3;
 	
 };
