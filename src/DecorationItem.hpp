fec04904ff14cd2d3b3dd0cdd1665816e5d64d57
Johannes Mattmann <johannes.mattmann@cern.ch>
2015-01-25 05:54:13 +0100

    DecorationItems-Bewegung entfernt

--- src/DecorationItem.hpp
+++ src/DecorationItem.hpp
@@ -7,4 +7,6 @@ class DecorationItem : public Item
 public:
 	DecorationItem(sf::Sprite* sprite, bool blocksPath, int texPosX, int texPosY, int texW, int texH);
 	void applyEffect();
+	
+	void update(sf::Time deltaTime);
 };
