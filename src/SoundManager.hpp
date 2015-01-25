d55cf566583260986361126b3d825597d17b780c
Johannes Mattmann <johannes.mattmann@cern.ch>
2015-01-25 04:10:31 +0100

    SoundManager verbessert und repariert

--- src/SoundManager.hpp
+++ src/SoundManager.hpp
@@ -12,12 +12,17 @@ class SoundManager : public AbstractManager
 {
 private:
 	std::map<std::string, sf::SoundBuffer *> bufferedSounds;
+	sf::Music bkgMusic;
 	void loadSound(std::string sndName);
+	std::vector<sf::Sound*> playingSound;
+	void tidyUp();
 	bool enableAudio;
 public:
 	void init(std::string preloadFileName);
 	const sf::SoundBuffer &getSound(std::string sndName);
 	SoundManager(bool enableAudio = true);
+	void playMusic(std::string fileName);
+	~SoundManager();
 	void playSound(std::string name);
 };
 
