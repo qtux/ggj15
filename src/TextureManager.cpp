b164bf59694769d8f3b87764c58f3bd416cd4093
Matthias Gazzari <matthias.gazzari@stud.tu-darmstadt.de>
2015-04-18 23:34:00 +0200

    improved and fixed texture manager

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,4 +1,5 @@
 #include "TextureManager.hpp"
+#include <algorithm>
 
 const sf::Texture& TextureManager::getTexture(std::string fileName, bool repeat, bool smooth)
 {
@@ -19,10 +20,10 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	// increase the size of the render texture by one in each direction
 	// this is required to re-center the tiles (the tiles appear to be
 	// shifted due to adding a border on the right and bottom side)
-	sf::Vector2u renderSize = gridSize + sf::Vector2u(1, 1);
+	sf::Vector2u vertexSize = gridSize + sf::Vector2u(1, 1);
 	
 	// create the render texture and clear it setting it to blue
-	if (!renderTex.create(renderSize.x * tileSize.x, renderSize.y * tileSize.y))
+	if (!renderTex.create(gridSize.x * tileSize.x, gridSize.y * tileSize.y))
 	{
 		return renderTex.getTexture();
 	}
@@ -31,12 +32,12 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	renderTex.setSmooth(smooth);
 	
 	// create a vertex array with one quad per tile and define edge positions
-	sf::VertexArray targetVertices(sf::Quads, renderSize.x * renderSize.y * 4);
-	for (auto i = 0; i < renderSize.x; ++i)
+	sf::VertexArray targetVertices(sf::Quads, vertexSize.x * vertexSize.y * 4);
+	for (unsigned int i = 0; i < vertexSize.x; ++i)
 	{
-		for (auto j = 0; j < renderSize.y; ++j)
+		for (unsigned int j = 0; j < vertexSize.y; ++j)
 		{
-			sf::Vertex* quad = &targetVertices[(i + j * renderSize.x) * 4];
+			sf::Vertex* quad = &targetVertices[(i + j * vertexSize.x) * 4];
 			quad[0].position = sf::Vector2f(i		* tileSize.x,	j		* tileSize.y) + offset;
 			quad[1].position = sf::Vector2f((i + 1)	* tileSize.x,	j		* tileSize.y) + offset;
 			quad[2].position = sf::Vector2f((i + 1)	* tileSize.x,	(j + 1)	* tileSize.y) + offset;
@@ -45,18 +46,16 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	}
 	
 	// draw basis tiles
-	for (auto i = 0; i < renderSize.x; ++i)
+	for (unsigned int i = 0; i < vertexSize.x; ++i)
 	{
-		for (auto j = 0; j < renderSize.y; ++j)
+		for (unsigned int j = 0; j < vertexSize.y; ++j)
 		{
-			sf::Vertex* quad = &targetVertices[(i + j * renderSize.x) * 4];
+			sf::Vertex* quad = &targetVertices[(i + j * vertexSize.x) * 4];
+			unsigned int x = std::min(i, gridSize.x - 1);
+			unsigned int y = std::min(j, gridSize.y - 1);
 			// define texture coordinates
-			unsigned int u = rand() % 6;									// column: random subtype
-			// use the adjacent left tile type for the right border
-			auto wrappedI = (i == gridSize.x) ? i - 1 : i;
-			// use the adjacent top tile type for the bottom border
-			auto wrappedJ = (j == gridSize.y) ? j - 1 : j;
-			unsigned int v = mapping[wrappedI + wrappedJ * gridSize.x];		// row: type identification
+			unsigned int u = rand() % 6;						// column: random subtype
+			unsigned int v = mapping[x + y * gridSize.x];		// row: type identification
 			quad[0].texCoords = sf::Vector2f(u			* tileSize.x,	v		* tileSize.y);
 			quad[1].texCoords = sf::Vector2f((u + 1)	* tileSize.x,	v		* tileSize.y);
 			quad[2].texCoords = sf::Vector2f((u + 1)	* tileSize.x,	(v + 1)	* tileSize.y);
@@ -66,17 +65,20 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	renderTex.draw(targetVertices, &baseTileSet);
 	
 	//draw top border tiles
-	for (auto i = 0; i < gridSize.x; ++i)
+	for (unsigned int i = 0; i < vertexSize.x; ++i)
 	{
-		for (auto j = 0; j < gridSize.y; ++j)
+		for (unsigned int j = 0; j < vertexSize.y; ++j)
 		{
-			sf::Vertex* quad = &targetVertices[(i + j * renderSize.x) * 4];
+			sf::Vertex* quad = &targetVertices[(i + j * vertexSize.x) * 4];
 			sf::Uint8 alpha = 0;
 			if (j > 0)
 			{
+				unsigned int x = std::min(i, gridSize.x - 1);
+				unsigned int y = std::min(j, gridSize.y - 1);
+				unsigned int yDec = std::min(j - 1, gridSize.y - 1);
 				// define texture coordinates and alpha
-				unsigned int v = mapping[i + (j - 1) * gridSize.x];		// row: type identification
-				alpha = (v != mapping[i + j * gridSize.x]) ? 255 : 0;
+				unsigned int v = mapping[x + yDec * gridSize.x];		// row: type identification
+				alpha = (v != mapping[x + y * gridSize.x]) ? 255 : 0;
 				unsigned int u = 6 + rand() % 4;						// column: random subtype
 				quad[0].texCoords = sf::Vector2f(u			* tileSize.x,	v		* tileSize.y);
 				quad[1].texCoords = sf::Vector2f((u + 1)	* tileSize.x,	v		* tileSize.y);
@@ -92,17 +94,20 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	renderTex.draw(targetVertices, &baseTileSet);
 	
 	// draw left border tiles
-	for (auto i = 0; i < gridSize.x; ++i)
+	for (unsigned int i = 0; i < vertexSize.x; ++i)
 	{
-		for (auto j = 0; j < gridSize.y; ++j)
+		for (unsigned int j = 0; j < vertexSize.y; ++j)
 		{
-			sf::Vertex* quad = &targetVertices[(i + j * renderSize.x) * 4];
+			sf::Vertex* quad = &targetVertices[(i + j * vertexSize.x) * 4];
 			sf::Uint8 alpha = 0;
 			if (i > 0)
 			{
+				unsigned int x = std::min(i, gridSize.x - 1);
+				unsigned int y = std::min(j, gridSize.y - 1);
+				unsigned int xDec = std::min(i - 1, gridSize.x - 1);
 				// define texture coordinates and alpha
-				unsigned int v = mapping[i  - 1 + j * gridSize.x];		// row: type identification
-				alpha = (v != mapping[i + j * gridSize.x]) ? 255 : 0;
+				unsigned int v = mapping[xDec + y * gridSize.x];		// row: type identification
+				alpha = (v != mapping[x + y * gridSize.x]) ? 255 : 0;
 				unsigned int u = 10 + rand() % 4;						// column: random subtype
 				quad[0].texCoords = sf::Vector2f(u			* tileSize.x,	v		* tileSize.y);
 				quad[1].texCoords = sf::Vector2f((u + 1)	* tileSize.x,	v		* tileSize.y);
@@ -118,18 +123,22 @@ const sf::Texture& TextureManager::getTileSet(const sf::Texture& baseTileSet, co
 	renderTex.draw(targetVertices, &baseTileSet);
 	
 	// draw top-left border tiles
-	for (auto i = 0; i < gridSize.x; ++i)
+	for (unsigned int i = 0; i < vertexSize.x; ++i)
 	{
-		for (auto j = 0; j < gridSize.y; ++j)
+		for (unsigned int j = 0; j < vertexSize.y; ++j)
 		{
 			// get a pointer to the current quad
-			sf::Vertex* quad = &targetVertices[(i + j * renderSize.x) * 4];
+			sf::Vertex* quad = &targetVertices[(i + j * vertexSize.x) * 4];
 			sf::Uint8 alpha = 0;
 			if (i > 0 && j > 0)
 			{
+				unsigned int x = std::min(i, gridSize.x - 1);
+				unsigned int y = std::min(j, gridSize.y - 1);
+				unsigned int xDec = std::min(i - 1, gridSize.x - 1);
+				unsigned int yDec = std::min(j - 1, gridSize.y - 1);
 				// define texture coordinates and alpha
-				unsigned int v = mapping[i - 1 + (j - 1) * gridSize.x];		// row: type identification
-				alpha = (v != mapping[i + (j - 1) * gridSize.x] || v != mapping[i - 1 + j * gridSize.x]) ? 255 : 0;
+				unsigned int v = mapping[xDec + yDec * gridSize.x];		// row: type identification
+				alpha = (v != mapping[x + yDec * gridSize.x] || v != mapping[xDec + y * gridSize.x]) ? 255 : 0;
 				unsigned int u = 14 + rand() % 2;							// column: random subtype
 				quad[0].texCoords = sf::Vector2f(u			* tileSize.x,	v		* tileSize.y);
 				quad[1].texCoords = sf::Vector2f((u + 1)	* tileSize.x,	v		* tileSize.y);
